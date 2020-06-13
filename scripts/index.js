9728d835c08341385fff05331360246cb3714137
lotar94 <lotar.94@gmail.com>
2020-06-13 18:59:11 -0400

    Se termina version mobile

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,22 @@
 function changeScrollView (id, page) {
     var elmnt = document.getElementById(id);
-    elmnt.scrollIntoView({block: "end", behavior: "smooth"});
-    const listItems = document.querySelectorAll('.header__menu li');
-    for (let i = 0; i < listItems.length; i++) {
-        if(listItems[i].textContent === page) {
-            listItems[i].style.opacity = '100%';
-        }else {
-            listItems[i].style.opacity = .5;
-        }
-    }
+    elmnt.scrollIntoView({block: "start", behavior: "smooth"});
+    // if(id === "home") {
+    //     setTimeout(() => {
+    //         document.getElementById('header').style.visibility = "unset";
+    //     }, 400);
+    // }
 }
+
+// window.addEventListener('scroll', function(e) {
+//     last_known_scroll_position = window.scrollY;
+//     if (last_known_scroll_position < 600) {
+//         document.getElementById('header').style.visibility = "hidden";
+//     } else {
+//         document.getElementById('header').style.visibility = "unset";
+//     }
+//     console.log('cambio=> ', last_known_scroll_position);
+    
+// });
 
 
